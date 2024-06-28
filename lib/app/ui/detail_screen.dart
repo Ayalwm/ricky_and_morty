5c454c9ed1a11833599ccd1fe9d58dee424d258f
Ayalwm <ayalworku.17@gmail.com>
2024-06-28 23:53:19 +0300

    intial release

--- lib/app/ui/detail_screen.dart
+++ lib/app/ui/detail_screen.dart
@@ -1,19 +1,221 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'dart:math';
 
-class DetailScreen extends StatelessWidget {
-  const DetailScreen({super.key, required this.id});
+void main() {
+  runApp(const MyApp());
+}
 
-  final String id;
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key});
 
   @override
   Widget build(BuildContext context) {
-    // accept the id
-    // Query detial with the id
-    // show the detal info
-    print("Detail Screen Id : $id");
+    return MaterialApp(
+      title: 'Rick and Morty Character Details',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key});
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(id),
+        title: const Text('Rick and Morty Characters'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const DetailScreen(id: '1'),
+              ),
+            );
+          },
+          child: const Text('Show Character Details'),
+        ),
+      ),
+    );
+  }
+}
+
+class DetailScreen extends StatelessWidget {
+  const DetailScreen({Key? key, required this.id}) : super(key: key);
+
+  final String id;
+
+  Future<Map<String, dynamic>?> fetchCharacterDetails() async {
+    try {
+      final response = await http
+          .get(Uri.parse('https://rickandmortyapi.com/api/character/$id'));
+
+      if (response.statusCode == 200) {
+        return json.decode(response.body);
+      } else {
+        throw Exception('Failed to load character details');
+      }
+    } catch (e) {
+      print('Error fetching character details: $e');
+      return null;
+    }
+  }
+
+  Future<List<dynamic>?> fetchEpisodeDetails(List<String> episodeUrls) async {
+    try {
+      final episodesData = await Future.wait(
+        episodeUrls.map((url) async {
+          final response = await http.get(Uri.parse(url));
+          if (response.statusCode == 200) {
+            final episodeData = json.decode(response.body);
+            final charactersUrls = List<String>.from(episodeData['characters']);
+            final random = Random();
+            final randomCharacterUrl =
+                charactersUrls[random.nextInt(charactersUrls.length)];
+            final characterResponse =
+                await http.get(Uri.parse(randomCharacterUrl));
+            if (characterResponse.statusCode == 200) {
+              final characterData = json.decode(characterResponse.body);
+              return {
+                'episode': episodeData['episode'],
+                'name': episodeData['name'],
+                'air_date': episodeData['air_date'],
+                'image': characterData['image'],
+              };
+            } else {
+              throw Exception('Failed to load character details for episode');
+            }
+          } else {
+            throw Exception('Failed to load episode details');
+          }
+        }),
+      );
+
+      return episodesData;
+    } catch (e) {
+      print('Error fetching episode details: $e');
+      return null;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Character Details"),
+      ),
+      body: FutureBuilder<Map<String, dynamic>?>(
+        future: fetchCharacterDetails(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else if (!snapshot.hasData || snapshot.data == null) {
+            return const Center(child: Text('No character details found.'));
+          } else {
+            final character = snapshot.data!;
+            return SingleChildScrollView(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Center(
+                    child: character['image'] != null
+                        ? Image.network(character['image'])
+                        : const SizedBox.shrink(),
+                  ),
+                  const SizedBox(height: 16),
+                  Text(
+                    character['name'] ?? 'Unknown',
+                    style: const TextStyle(
+                        fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(height: 8),
+                  Row(
+                    children: [
+                      const Icon(Icons.person),
+                      const SizedBox(width: 4),
+                      Text(character['species'] ?? 'Unknown'),
+                      const SizedBox(width: 16),
+                      const Icon(Icons.male),
+                      const SizedBox(width: 4),
+                      Text(character['gender'] ?? 'Unknown'),
+                    ],
+                  ),
+                  const SizedBox(height: 8),
+                  Row(
+                    children: [
+                      const Icon(Icons.location_on),
+                      const SizedBox(width: 4),
+                      Text(character['location']['name'] ?? 'Unknown'),
+                    ],
+                  ),
+                  const SizedBox(height: 16),
+                  const Text(
+                    'Episodes:',
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(height: 8),
+                  FutureBuilder<List<dynamic>?>(
+                    future: fetchEpisodeDetails(
+                        List<String>.from(character['episode'] ?? [])),
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return const Center(child: CircularProgressIndicator());
+                      } else if (snapshot.hasError) {
+                        return Center(child: Text('Error: ${snapshot.error}'));
+                      } else if (!snapshot.hasData || snapshot.data == null) {
+                        return const Center(child: Text('No episodes found.'));
+                      } else {
+                        final episodes = snapshot.data!;
+                        return Column(
+                          children: episodes.map((episode) {
+                            return Card(
+                              child: ListTile(
+                                leading: episode['image'] != null
+                                    ? Image.network(
+                                        episode['image'],
+                                        width: 50,
+                                        height: 50,
+                                        fit: BoxFit.cover,
+                                      )
+                                    : const SizedBox.shrink(),
+                                title: Text(
+                                  episode['episode'] != null
+                                      ? 'S${episode['episode'].toString().padLeft(2, '0')}'
+                                      : 'Unknown',
+                                  style: const TextStyle(
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                subtitle: Text(
+                                  episode['name'] ?? 'Unknown',
+                                  style: const TextStyle(
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                trailing: Text(
+                                  ' ${episode['air_date'] ?? 'Unknown'}',
+                                ),
+                              ),
+                            );
+                          }).toList(),
+                        );
+                      }
+                    },
+                  ),
+                ],
+              ),
+            );
+          }
+        },
       ),
     );
   }
--- lib/app/ui/home_screen.dart
+++ lib/app/ui/home_screen.dart
@@ -5,13 +5,33 @@ import 'package:rick_and_morty/app/utils/query.dart';
 import 'package:rick_and_morty/app/widgets/character_widget.dart';
 
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
+  const HomeScreen({Key? key});
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  TextEditingController _searchController = TextEditingController();
+  String _searchText = "";
+  String? _selectedGender;
+
+  @override
+  void initState() {
+    super.initState();
+    _searchController.addListener(() {
+      setState(() {
+        _searchText = _searchController.text;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _searchController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,91 +40,159 @@ class _HomeScreenState extends State<HomeScreen> {
           "assets/logo.png",
           height: 62,
         ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: SizedBox(
+              width: 150,
+              height: 36,
+              child: TextField(
+                controller: _searchController,
+                decoration: InputDecoration(
+                  hintText: 'Search',
+                  prefixIcon: Icon(Icons.search, color: Colors.grey),
+                  contentPadding: EdgeInsets.symmetric(vertical: 0),
+                  isDense: true,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8.0),
+                    borderSide: BorderSide(color: Colors.grey),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8.0),
+                    borderSide: BorderSide(color: Colors.grey),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8.0),
+                    borderSide: BorderSide(color: Colors.grey),
+                  ),
+                ),
+                style: TextStyle(color: Colors.black87),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: SizedBox(
+              width: 140,
+              height: 36,
+              child: DropdownButton<String>(
+                hint:
+                    const Text("Gender", style: TextStyle(color: Colors.grey)),
+                value: _selectedGender,
+                isDense: true,
+                items: <String>['All', 'Male', 'Female', 'unknown']
+                    .map((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value == 'All' ? null : value,
+                    child: Text(value),
+                  );
+                }).toList(),
+                onChanged: (newValue) {
+                  setState(() {
+                    _selectedGender = newValue;
+                  });
+                },
+                underline: Container(),
+                icon: Icon(Icons.arrow_drop_down, color: Colors.grey),
+                style: TextStyle(color: Colors.black87, fontSize: 16),
+                dropdownColor: Colors.white,
+                borderRadius: BorderRadius.circular(8),
+              ),
+            ),
+          ),
+        ],
       ),
       body: SafeArea(
         child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16),
-          child: Query(
-            builder: (result, {fetchMore, refetch}) {
-              // We have data
-              if (result.data != null) {
-                int? nextPage = 1;
-                List<Character> characters =
-                    (result.data!["characters"]["results"] as List)
-                        .map((e) => Character.fromMap(e))
-                        .toList();
+          padding: const EdgeInsets.symmetric(vertical: 16),
+          child: Column(
+            children: [
+              Expanded(
+                child: Query(
+                  options: QueryOptions(
+                    fetchPolicy: FetchPolicy.cacheAndNetwork,
+                    document: getAllCharachters(),
+                    variables: const {"page": 1},
+                  ),
+                  builder: (result, {fetchMore, refetch}) {
+                    if (result.data != null) {
+                      int? nextPage = 1;
+                      List<Character> characters =
+                          (result.data!["characters"]["results"] as List)
+                              .map((e) => Character.fromMap(e))
+                              .toList();
 
-                nextPage = result.data!["characters"]["info"]["next"];
+                      nextPage = result.data!["characters"]["info"]["next"];
 
-                return RefreshIndicator(
-                  onRefresh: () async {
-                    await refetch!();
-                    nextPage = 1;
-                  },
-                  child: SingleChildScrollView(
-                    child: Column(
-                      children: [
-                        Center(
-                          child: Wrap(
-                            spacing: 8,
-                            runSpacing: 8,
-                            children: characters
-                                .map((e) => CharacterWidget(character: e))
-                                .toList(),
+                      List<Character> filteredCharacters = characters
+                          .where((character) =>
+                              character.name
+                                  .toLowerCase()
+                                  .contains(_searchText.toLowerCase()) &&
+                              (_selectedGender == null ||
+                                  character.gender.toLowerCase() ==
+                                      _selectedGender!.toLowerCase()))
+                          .toList();
+
+                      return RefreshIndicator(
+                        onRefresh: () async {
+                          await refetch!();
+                          nextPage = 1;
+                        },
+                        child: SingleChildScrollView(
+                          child: Column(
+                            children: [
+                              Center(
+                                child: Wrap(
+                                  spacing: 8,
+                                  runSpacing: 8,
+                                  children: filteredCharacters
+                                      .map((e) => CharacterWidget(character: e))
+                                      .toList(),
+                                ),
+                              ),
+                              const SizedBox(height: 16),
+                              if (nextPage != null)
+                                ElevatedButton(
+                                    onPressed: () async {
+                                      FetchMoreOptions opts = FetchMoreOptions(
+                                        variables: {'page': nextPage},
+                                        updateQuery: (previousResultData,
+                                            fetchMoreResultData) {
+                                          final List<dynamic> repos = [
+                                            ...previousResultData!["characters"]
+                                                ["results"] as List<dynamic>,
+                                            ...fetchMoreResultData![
+                                                    "characters"]["results"]
+                                                as List<dynamic>
+                                          ];
+                                          fetchMoreResultData["characters"]
+                                              ["results"] = repos;
+                                          return fetchMoreResultData;
+                                        },
+                                      );
+                                      await fetchMore!(opts);
+                                    },
+                                    child: result.isLoading
+                                        ? const CircularProgressIndicator
+                                            .adaptive()
+                                        : const Text("Load More"))
+                            ],
                           ),
                         ),
-                        const SizedBox(
-                          height: 16,
-                        ),
-                        if (nextPage != null)
-                          ElevatedButton(
-                              onPressed: () async {
-                                FetchMoreOptions opts = FetchMoreOptions(
-                                  variables: {'page': nextPage},
-                                  updateQuery: (previousResultData,
-                                      fetchMoreResultData) {
-                                    final List<dynamic> repos = [
-                                      ...previousResultData!["characters"]
-                                          ["results"] as List<dynamic>,
-                                      ...fetchMoreResultData!["characters"]
-                                          ["results"] as List<dynamic>
-                                    ];
-                                    fetchMoreResultData["characters"]
-                                        ["results"] = repos;
-                                    return fetchMoreResultData;
-                                  },
-                                );
-                                await fetchMore!(opts);
-                              },
-                              child: result.isLoading
-                                  ? const CircularProgressIndicator.adaptive()
-                                  : const Text("Load More"))
-                      ],
-                    ),
-                  ),
-                );
-              }
-              // We got data but it is null
-              else if (result.data == null) {
-                return const Text("Data Not Found!");
-              }
-              // We don't have data yet -> LOADING STATE
-              else if (result.isLoading) {
-                return const Center(
-                  child: Text("Loading..."),
-                );
-              }
-              // error state
-              else {
-                return const Center(
-                  child: Center(child: Text("Something went wrong")),
-                );
-              }
-            },
-            options: QueryOptions(
-                fetchPolicy: FetchPolicy.cacheAndNetwork,
-                document: getAllCharachters(),
-                variables: const {"page": 1}),
+                      );
+                    } else if (result.data == null) {
+                      return const Text("Data Not Found!");
+                    } else if (result.isLoading) {
+                      return const Center(child: Text("Loading..."));
+                    } else {
+                      return const Center(
+                          child: Center(child: Text("Something went wrong")));
+                    }
+                  },
+                ),
+              ),
+            ],
           ),
         ),
       ),
